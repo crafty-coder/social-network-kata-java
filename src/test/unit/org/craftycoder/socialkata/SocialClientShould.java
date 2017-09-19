6de44cc3c39561f396c5f696fd2f37f3b62cb5d0
carlos <carlos.pena@192.com>
2017-09-19 23:50:43 +0100

    Add Test "send_two_publish_commands_to_publish_to_timeline" (red)
    - Test Added in SocialClientShould

--- src/test/unit/org/craftycoder/socialkata/SocialClientShould.java
+++ src/test/unit/org/craftycoder/socialkata/SocialClientShould.java
@@ -6,9 +6,7 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class SocialClientShould {
@@ -26,11 +24,12 @@ public class SocialClientShould {
                 .thenReturn("exit")
                 .thenThrow(new RuntimeException("Not Expected call"));
 
-        new SocialClient(consoleMock,publishToTimelineMock).start();
+        new SocialClient(consoleMock, publishToTimelineMock).start();
 
         verify(consoleMock, times(1)).read();
 
     }
+
     @Test
     public void send_publish_commands_to_publish_to_timeline() {
 
@@ -38,12 +37,26 @@ public class SocialClientShould {
                 .thenReturn("Alice -> I love the weather today")
                 .thenReturn("exit");
 
-        new SocialClient(consoleMock,publishToTimelineMock).start();
+        new SocialClient(consoleMock, publishToTimelineMock).start();
 
-        verify(publishToTimelineMock,times(1)).publish("Alice","I love the weather today");
+        verify(publishToTimelineMock, times(1)).publish("Alice", "I love the weather today");
 
     }
 
+    @Test
+    public void send_two_publish_commands_to_publish_to_timeline() {
+
+        when(consoleMock.read())
+                .thenReturn("Alice -> I love the weather today")
+                .thenReturn("Bob -> Damn! We lost!")
+                .thenReturn("exit");
+
+        new SocialClient(consoleMock, publishToTimelineMock).start();
+
+        verify(publishToTimelineMock, times(1)).publish("Alice", "I love the weather today");
+        verify(publishToTimelineMock, times(1)).publish("Bob", "Damn! We lost!");
+
+    }
 
 
 }
