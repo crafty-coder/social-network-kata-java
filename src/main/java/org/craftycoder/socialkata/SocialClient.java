758adf240f40bb1e2074074ba7f74153faa8b1f6
crafty-coder <thefeared@gmail.com>
2017-09-22 20:17:32 +0100

    Implement "send_two_publish_commands_to_publish_to_timeline" (green)

--- src/main/java/org/craftycoder/socialkata/SocialClient.java
+++ src/main/java/org/craftycoder/socialkata/SocialClient.java
@@ -2,9 +2,14 @@ package org.craftycoder.socialkata;
 
 import org.craftycoder.socialkata.actions.PublishToTimeline;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class SocialClient {
 
     private static final String EXIT = "exit";
+    private static final Pattern userPattern = Pattern.compile("^(.+)\\s->.*$");
+    private static final Pattern msgPattern = Pattern.compile("^.+\\s->\\s(.+)$");
 
     private final Console console;
     private final PublishToTimeline publishToTimeline;
@@ -16,16 +21,34 @@ public class SocialClient {
 
     void start() {
         String textRead;
-        do{
+        do {
             textRead = console.read();
             ifActionThenDispatch(textRead);
-
-        }while (!EXIT.equalsIgnoreCase(textRead));
+        } while (!EXIT.equalsIgnoreCase(textRead));
     }
 
-    private void ifActionThenDispatch(String text){
-        if (!EXIT.equalsIgnoreCase(text)){
-            publishToTimeline.publish("Alice","I love the weather today");
+    private void ifActionThenDispatch(String text) {
+        if (!EXIT.equalsIgnoreCase(text)) {
+            String user = userExtractor(text);
+            String msg = msgExtractor(text);
+            publishToTimeline.publish(user, msg);
         }
+    }
+
+    private String userExtractor(String text) {
+        return extract(text, userPattern);
+    }
+
+    private String msgExtractor(String text) {
+        return extract(text, msgPattern);
+
+    }
+
+    private String extract(String text, Pattern msgPattern) {
+        Matcher m =  msgPattern.matcher(text);
+        if (m.find()){
+            return m.group(1);
+        }
+        return "";
     }
 }
